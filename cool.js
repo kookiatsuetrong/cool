1fd32b3122402b6d887f7dcae14a9fd9831a390c
Kookiat Suetrong <kookiatsuetrong@gmail.com>
2015-04-01 16:38:06 +0700

    Update

--- cool.js
+++ cool.js
@@ -31,19 +31,25 @@ function request(url) {
 function transpile(originalCode) {
 	var code = originalCode;
 
+	// remove all comments // /* */
+	code = code.replace(/\/\/.*\n/g, "\n");
+	code = code.replace(/\/\*[\w\'\s\r\n\*]*\*\//g, "");
+
 	// replacing field
 	code = code.replace(/field(\s+)/g, "this.");
+	code = code.replace(/property(\s+)/g, "this.");
 
 	// replacing method
 	code = code.replace(/method(\s+)(\w+)/g, "this.$2 = function ");
 
 	// replacing class without constructor
-	code = code.replace(/class(\s+)(\w+)(\s+){/g, "function $2 () { ");
+	code = code.replace(/class(\s+)(\w+)(\s*){/g, "function $2 () { ");
 
 	// replacing class with constructor
 	code = code.replace(/class(\s+)(\w+)/g, "function $2 ");
 
 	// remove constructor
+	code = code.replace(/constructor(\s+)/g, '');
 	code = code.replace(/constructor/g, '');
 
 	if (originalCode.match(/class(\s+)Main/))
--- transpile.js
+++ transpile.js
@@ -1,23 +1,35 @@
+/*
+public  item -> this.item
+private item -> var item
+
+public  method sum() -> this.sum = function()
+private method sum() -> function sum()
+
+*/
+
 function transpile(originalCode) {
 	var code = originalCode;
 
+	// remove all comments // /* */
+	code = code.replace(/\/\/.*\n/g, "\n");
+	code = code.replace(/\/\*[\w\'\s\r\n\*]*\*\//g, "");
+
 	// replacing field
 	code = code.replace(/field(\s+)/g, "this.");
+	code = code.replace(/property(\s+)/g, "this.");
 
 	// replacing method
 	code = code.replace(/method(\s+)(\w+)/g, "this.$2 = function ");
 
 	// replacing class without constructor
-	code = code.replace(/class(\s+)(\w+)(\s+){/g, "function $2 () { ");
+	code = code.replace(/class(\s+)(\w+)(\s*){/g, "function $2 () { ");
 
 	// replacing class with constructor
 	code = code.replace(/class(\s+)(\w+)/g, "function $2 ");
 
 	// remove constructor
+	code = code.replace(/constructor(\s+)/g, '');
 	code = code.replace(/constructor/g, '');
-
-	// add ; to the end of }
-	// code = code.replace(/}/g, "};");
 
 	if (originalCode.match(/class(\s+)Main/))
 		code += "\n;\n(new Main()).start();\n";
@@ -31,7 +43,7 @@ function main() {
 	process.stdin.on("data", function (input) { stream += input; });
 	process.stdin.on("end", function () {
 		var fs = require('fs');
-		stream += "\n" + fs.readFileSync("lib.cool");
+		stream += "\n" + fs.readFileSync("library.cool");
 		console.log(transpile(stream));
 	});
 }
