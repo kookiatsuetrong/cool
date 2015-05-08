0a0dbdc3d9f247559a22810ac3e38d4e25aad9ba
Kookiat Suetrong <kookiatsuetrong@gmail.com>
2015-05-08 19:54:59 +0700

    Update

--- cool.js
+++ cool.js
@@ -200,17 +200,17 @@ function File (name) {
 	Root.call(this);
 	this.class = "File";
 	this.name = name;
-	this.read = function() {
-		return "";
-	}
-	this.write = function(data) {
-	}
+	this.read = function() { return ""; }
+	this.write = function(data) {}
+	this.exists = function() { return false; }
+	this.list = function() { return []; }
+	this.isDirectory = function() { return false; }
 }
+
 function TextFile (name) {
 	File.call(this);
 	this.class = "TextFile";
-	this.append = function(data) {
-	}
+	this.append = function(data) {}
 }
 
 function Float() {
@@ -224,27 +224,3 @@ function Integer() {
 		return parseInt(x);
 	}
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-//
